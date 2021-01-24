8962f0c4be3426d08871f354558843d4dfac4a60
f3ve <free.eve@pm.me>
2021-01-24 13:37:30 -0800

    fixed database connection issue

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 require('dotenv').config({ path: './.env' });
 console.log(process.env.PASSWORD);
+console.log(process.env.DB_USER);
 const connectDB = mysql.createConnection({
   host: `${process.env.HOST}`,
-  user: `${process.env.USERNAME}`,
+  user: `${process.env.DB_USER}`,
   password: `${process.env.PASSWORD}`,
 });
 
