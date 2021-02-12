4a90b1db5ca5a0750261fd4716f02d124eb92f76
rmiglin <rossmiglin@gmail.com>
2021-02-12 02:35:13 -0500

    search button functional

---
+++ frontend/src/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Search.css';
+
+function Search() {
+    return(
+        <div>
+            <h1>Ross was here</h1>
+        </div>
+    );
+}
+
+export default Search;
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Search from './Search';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+        <Route exact path="/" component={App}/>
+        <Route path="/search" component={Search}/>
+        {/* <App /> */}
+        {/* <Search /> */}
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
@@ -15,3 +22,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
