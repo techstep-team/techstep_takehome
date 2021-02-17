da46380cce7e937a74447db30d3bc57bba26e46c
Chuck Thompson <chuckthompson27@me.com>
2021-02-16 18:17:20 -0800

    Refactor app for navigation.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import { Layout, Row, Card, Col, Button } from 'antd';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import logo_green from './logo_green.png';
-import TeamPic from './TeamPic.png';
-const { Header, Content, Footer } = Layout;
+
+import Home from './components/Home';
+import Search from './components/Search';
+
+const { Header, Footer } = Layout;
 
 function App() {
   return (
@@ -15,92 +19,10 @@ function App() {
           background: 'white',
         }}
       >
-        <img className='techstep-logo' src={logo_green} alt='logo' />
+        <img className="techstep-logo" src={logo_green} alt="Techstep logo" />
       </Header>
-      <Content style={{ backgroundColor: 'white', paddingBottom: '200px' }}>
-        <Row className='card-container'>
-          <Card style={{ width: '1500px', height: '280px' }}>
-            <Row className='title-container'>
-              <h1 className='title' style={{textAlign: 'center'}}>
-                Welcome to the Techstep <br/> takehome assessment! <br/>
-              </h1>
-            </Row>
-            <Row className='title-button'>
-              <Button
-                style={{
-                  width: '298px',
-                  height: '45px',
-                  background: '#00AE8C',
-                  borderRadius: '4.5px',
-                  border: 'none',
-                }}
-              >
-                <span className='buttonClick'>To Your Page</span>
-              </Button>
-            </Row>
-          </Card>
-        </Row>
-        <br />
-        <Row justify='center'>
-          <Card
-            style={{
-              width: 1350,
-              height: 500,
-              background: '#ffffff',
-              borderRadius: '32px',
-              border: 'none',
-            }}
-          >
-            <Row justify='center'>
-              <Col style={{ paddingRight: 80 }}>
-                <Card
-                  style={{
-                    width: 650,
-                    backgroundColor: 'transparent',
-                    borderColor: 'transparent',
-                    border: 'none',
-                  }}
-                >
-                  <Row justify='start'>
-                    <span className='ourStoryTitle'>Our Story</span>
-                  </Row>
-                  <br />
-                  <Row>
-                    <span className='ourStoryText'>
-                      We're helping the world techstep forward one business at a
-                      time. As a one-stop partner, we find and implement a
-                      personalized tech solution that solves problems,
-                      alleviates pain points and digitally transforms growing
-                      businesses.
-                    </span>
-                    <span className='ourStoryText'>
-                      Technology transformations are complex and overwhelming.
-                      That's where we come in. We relentlessly research and
-                      uncover the most efficient technology tools, and are
-                      committed to developing the best-unbiased recommendations.
-                      We guide businesses every step of the way from
-                      recommendation, implementation, to continued support.
-                    </span>
-                    <span className='ourStoryText'>
-                      Welcome to the techstep revolution.
-                    </span>
-                  </Row>
-                </Card>
-              </Col>
-              <Col>
-                <img
-                  alt=''
-                  src={TeamPic}
-                  style={{
-                    width: 490,
-                    paddingTop: '80px',
-                  }}
-                ></img>
-              </Col>
-            </Row>
-          </Card>
-        </Row>
-      </Content>
+      <Route path="/search" component={Search} />
+      <Route exact path="/" component={Home} />
       <Footer style={{ backgroundColor: '#182628', height: '200px' }}>
         <Card
           style={{
@@ -111,9 +33,9 @@ function App() {
         >
           <center>
             <img
-              className='techstep-logo'
+              className="techstep-logo"
               src={logo_green}
-              alt='Techstep Logo'
+              alt="Techstep Logo"
             />
             <br />
             <br />
