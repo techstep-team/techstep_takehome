ea46be7932d9e8f964b3b7242644d894162c1061
Bryce Champaign <brycechampaign@gmail.com>
2021-02-18 22:57:04 -0500

    Add error handling when fetching results from API

--- frontend/src/pages/Search/index.js
+++ frontend/src/pages/Search/index.js
@@ -8,13 +8,22 @@ const Search = () => {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [isFirstSearch, setIsFirstSearch] = useState(true);
+  const [error, setError] = useState(false);
 
   const handleSearch = async term => {
+    setError(false);
     setIsFirstSearch(false);
     setLoading(true);
-    const newResults = await searchUserByName(term);
-    setResults(newResults);
-    setLoading(false);
+
+    try {
+      const newResults = await searchUserByName(term);
+      setResults(newResults);
+      setLoading(false);
+    } catch (error) {
+      setResults([]);
+      setError(true);
+      setLoading(false);
+    }
   }
 
   return (
@@ -23,7 +32,12 @@ const Search = () => {
       <Searchbar handleSearch={handleSearch}/>
       <div id="results-container">
         {loading ? <p id="loading-text">Loading...</p> : results.map(user => <UserCard user={user} key={user.id} />)}
-        {!isFirstSearch && results.length === 0 && !loading ? <p>No results found</p> : null}
+        {!isFirstSearch && 
+        results.length === 0 && 
+        !loading &&
+        !error ? 
+        <p>No results found</p> : null}
+        {error ? <p>There was a problem fetching your results. Please try again.</p> : null}
       </div>
     </div>
   )
