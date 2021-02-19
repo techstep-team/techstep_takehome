9b54688cfb58e9f59ce3b8e71b425f2be46bd127
Bryce Champaign <brycechampaign@gmail.com>
2021-02-18 22:50:25 -0500

    Implement search page
    The Search page displays a search bar for searching for users based on
    name. It renders the results below the search bar.

--- frontend/src/pages/Search/index.js
+++ frontend/src/pages/Search/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Search.css';
+import { searchUserByName } from '../../APIHelpers';
+import Searchbar from './components/Searchbar';
+import UserCard from './components/UserCard';
 
 const Search = () => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isFirstSearch, setIsFirstSearch] = useState(true);
+
+  const handleSearch = async term => {
+    setIsFirstSearch(false);
+    setLoading(true);
+    const newResults = await searchUserByName(term);
+    setResults(newResults);
+    setLoading(false);
+  }
+
   return (
-    <div></div>
+    <div id="search-container">
+      <h1>Search our users</h1>
+      <Searchbar handleSearch={handleSearch}/>
+      <div id="results-container">
+        {loading ? <p id="loading-text">Loading...</p> : results.map(user => <UserCard user={user} key={user.id} />)}
+        {!isFirstSearch && results.length === 0 && !loading ? <p>No results found</p> : null}
+      </div>
+    </div>
   )
 };
 
